eff61a09edd14b95d2c9ddb49aa00dbd80eaab2e
auorauh <dr.teasdale.trenton@gmail.com>
2022-01-08 21:20:22 -0700

    csp test with no inline

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ function openNav() {
   }
   function closeNav() {
     document.getElementById("navSide").style.width = "0";
-  }  
+  }
+const span = document.querySelector('#openNav');
+span.addEventListener('click', () => {
+  openNav();
+});
+const xBtn = document.querySelector('#xBtn');
+xBtn.addEventListener('click', () => {
+  closeNav();
+});
